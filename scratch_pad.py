5e7157eaaad877f173cf8f9f61b03c1d44ee300b
Ryan Martin <rjmartin73@gmail.com>
2025-02-17 12:07:43 -0500

    Added wire file logic

--- scratch_pad.py
+++ scratch_pad.py
@@ -16,6 +16,25 @@ size_patterns = {
     '2"': re.compile(r'(?<!\d)2[”|"|in| |.]|(?<!\d)2$'),
     '1"': re.compile(r'(?<!\d)1[”|"|in| |.]|(?<!\d)1$'),
 }
+wire_sizes = {
+    "4/0": re.compile(r'#4'),
+    "3/0": re.compile(r''),
+    "2/0": re.compile(r''),
+    "1/0": re.compile(r''),
+    "#250": re.compile(r''),
+    "#300": re.compile(r''),
+    "#350": re.compile(r''),
+    "#400": re.compile(r''),
+    "#500": re.compile(r''),
+    "#600": re.compile(r''),
+    "#750": re.compile(r''),
+    "#14": re.compile(r'(\b#*14)\s?(awg)*[-/]?\b'),
+    "#12": re.compile(r'(\b#*14)\s?(awg)*[-/]?\b'),
+    "#10": re.compile(r'(\b#*14)\s?(awg)*[-/]?\b'),
+    "#8": re.compile(r'(\b#*14)\s?(awg)*[-/]?\b'),
+    "#6": re.compile(r'(\b#*14)\s?(awg)*[-/]?\b'),
+    "LOW VOLTAGE": re.compile(r''),    
+}
 
 df = pd.read_csv("./assets/conduit_types.csv")
 # print (df.describe())
@@ -30,6 +49,13 @@ def get_conduit_size(description):
             return f'{size}'  # ✅ Correctly returns the first match
     return 'UNK'  # ✅ Returns UNK only if no match is found
 
+
+def get_wire_guage(description):
+    description = description.lower()
+    for guage, pattern in wire_sizes.items():
+        if pattern.search(description):
+            return f'{guage}' # ✅ Correctly returns the first match
+    return 'UNK'  # ✅ Returns UNK only if no match is found 
 
 df["ConduitDiameter"] = df["Description"].apply(get_conduit_size)
 # def size_conduit():
